604502c71b3a7cc0e9c86dd189a9ea87656b8891
jeanrod98 <jcrod.jc@gmail.com>
2022-02-27 20:25:13 -0500

    subida de cambios

---
+++ src/components/Form.js
@@ -0,0 +1,100 @@
+import React, { useState } from 'react'
+import styled from "styled-components";
+import {useNavigate} from 'react-router-dom'
+
+function Form() {
+    const navigate = useNavigate();
+
+    // Estado para guardar el nombre
+    const [nombre, setNombre] = useState();
+    //llenar nombre en el estado
+    
+
+    const goToPage = (e) => {
+        e.preventDefault();
+        
+        if(nombre === '' || nombre == undefined ){
+            alert('NOMBRE INCORRECTO, vuelva a intentar!')
+        }else{
+
+            localStorage.setItem('nameUser', nombre);
+            // console.log('correcto');
+            navigate('/nameBatman');
+        }
+    }
+  return (
+    <ContenedorForm>
+        <InputDiv>
+            <InputText placeholder='John Doe' autoFocus type='text' onChange={e => setNombre(e.target.value)}>
+            </InputText>
+            <label>* Ingrese un nombre y un apellido.</label>
+        </InputDiv>
+        <ContenedorBoton>
+
+            <BotonGenerar onClick={goToPage}>
+                Generar Nombre
+            </BotonGenerar>
+        </ContenedorBoton>
+
+    </ContenedorForm>
+  )
+}
+
+export default Form
+
+const ContenedorForm = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: #000;
+    min-height: 100vh;
+    flex-direction: column;
+`;
+
+const ContenedorBoton = styled.div`
+    margin-top: 30px;
+       
+`;
+const BotonGenerar = styled.button`
+    width: 200px;
+    height: 40px;
+    background: transparent;
+    color: #cd0a0e;
+    text-transform: uppercase;
+    border: 1px solid white;
+    border-radius: 5px;
+    font-weight: bold;
+
+    &:hover{
+        background: #fff;
+        color: #cd0a0e;
+        border: 0;
+        cursor: pointer;
+    }
+   
+`;
+const InputDiv = styled.div`
+// width: 100%;
+display: flex;
+flex-direction: column;
+label{
+    margin-top: 5px;
+    color: #fff;
+    font-size: 12px;
+}
+       
+`;
+const InputText = styled.input`
+    color: #cd0a0e;
+    width: 450px;
+    height: 50px;
+    font-size: 34px;
+    outline: none;
+    background: transparent;
+    border: 0;
+    border-bottom: 1px solid red;
+    text-transform: uppercase;
+    text-align: center;
+    
+
+`;
---
+++ src/components/Inicio.js
@@ -0,0 +1,111 @@
+import React, { useEffect } from "react";
+import styled from "styled-components";
+import imagen from '../assets/batman1.jpg';
+import html2canvas from 'html2canvas';
+import {useNavigate} from 'react-router-dom';
+
+function Inicio() {
+    const navigate = useNavigate();
+
+    const nombreUsuario = localStorage.getItem('nameUser');
+
+    //Funcion para capturar el div y descargar en imagen
+    // const TestButton = () => {
+    //     html2canvas(document.getElementById('conetenedor-texto'))
+    //     .then(canvas => {
+    //     //   canvas.toDataURL()
+    //       let enlace = document.createElement('a')
+    //   enlace.download = `${nombreUsuario}.jpg`
+    //   // Convertir la imagen a Base64
+    //   enlace.href = canvas.toDataURL()
+    //   // Hacer click en él
+    //   enlace.click()
+    // });
+// }
+//Ir atras
+const atras = (e) => {
+e.preventDefault();
+navigate('/');
+}
+  return (
+    <Contenedor>
+        <Atras>
+
+            <ion-icon onClick={atras} name="arrow-back-circle-outline"></ion-icon>
+        </Atras>
+        <ContenedorTexto id="conetenedor-texto">
+
+            <Texto>{nombreUsuario}</Texto>
+        </ContenedorTexto>
+        {/* <button onClick={TestButton}>Download</button> */}
+      {/* <Canvas id="myCanvas">Hola</Canvas> */}
+    </Contenedor>
+  );
+}
+
+export default Inicio;
+const Contenedor = styled.div`
+    background: #000;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+   
+    ion-icon{
+        color: #fff;
+        font-size: 42px;
+    }
+`;
+const Atras = styled.div`
+    justify-content: felx-start;
+    // background: red;  
+    width: 80%; 
+    height: 80px;
+    ion-icon{
+        color: #fff;
+        font-size: 42px;
+    }
+    ion-icon:hover{
+        color: red;
+        cursor: pointer;
+    }
+`;
+
+const ContenedorTexto = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 1px solid red;
+    width: 80%;
+    height: 300px;
+    border-radius: 20px;
+
+    @media screen and (max-width: 800px){
+        height: 100px;
+    }
+`;
+const Texto = styled.h1`
+// @import url('https://fonts.googleapis.com/css2?family=Anton&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@700&display=swap');
+    // font-family: 'Anton', sans-serif;
+    font-family: 'League Spartan', sans-serif;
+    width: 100%;
+    
+    
+    margin: 0;
+    font-size: 70px;
+    color: transparent; //#000
+    text-align: center;
+    text-transform: uppercase;
+    background-image: url(${imagen});
+    background-size: cover;
+    background-position-y: center;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+@media screen and (max-width: 800px){
+    font-size: 26px;
+}
+`;
